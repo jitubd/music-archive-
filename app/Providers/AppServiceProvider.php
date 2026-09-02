d2217dc62a6171ae124efcd6dd22dc9ec0120335
jitubd <jitu.bd@live.com>
2026-09-02 11:28:09 +0600

    Force HTTPS for all URL generation to prevent insecure-form warnings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_FORCE_HTTPS', true)) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
